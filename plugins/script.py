7ed10822bae675dbb3da4c243ea78f1cf5905ead
nischayydv <nischay419@gmail.com>
2025-06-28 11:05:18 +0530

    Update script.py

--- plugins/script.py
+++ plugins/script.py
@@ -1,3 +1,4 @@
+# ⚠️ Credit: Developed by @NY_BOTS | Support: @NY_BOTS_SUPPORT | Channel: @NY_BOTS
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 class Translation(object):
@@ -9,37 +10,33 @@ class Translation(object):
 **Sᴇɴᴅ ᴍᴇ ᴀ ᴅɪʀᴇᴄᴛ ʟɪɴᴋ ᴀɴᴅ ɪ ᴡɪʟʟ ᴜᴘʟᴏᴀᴅ ɪᴛ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ᴀs ᴀ ꜰɪʟᴇ/ᴠɪᴅᴇᴏ**
 
 Usᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ
-
 """
-    
 
     HELP_TEXT = """
 **Hᴏᴡ Tᴏ Usᴇ Tʜɪs Bᴏᴛ** 🤔
-   
-𖣔 Fɪʀsᴛ ɢᴏ ᴛᴏ ᴛʜᴇ /settings ᴀɴᴅ ᴄʜᴀɴɢᴇ ᴛʜᴇ ʙᴏᴛ ʙᴇʜᴀᴠɪᴏʀ ᴀs ʏᴏᴜʀ ᴄʜᴏɪᴄᴇ.
 
-𖣔 Sᴇɴᴅ ᴍᴇ ᴛʜᴇ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ᴛᴏ sᴀᴠᴇ ɪᴛ ᴘᴇʀᴍᴀɴᴇɴᴛʟʏ.
+𖣔 Fɪʀsᴛ ɢᴏ ᴛᴏ ᴛʜᴇ /settings ᴀɴᴅ ᴄʜᴀɴɢᴇ ᴛʜᴇ ʙᴏᴛ ʙᴇʜᴀᴠɪᴏʀ ᴀs ʏᴏᴜʀ ᴄʜᴏɪᴄᴇ。
+
+𖣔 Sᴇɴᴅ ᴍᴇ ᴛʜᴇ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ᴛᴏ sᴀᴠᴇ ɪᴛ ᴘᴇʀᴍᴀɴᴇɴᴛʟʏ。
 
 𖣔 **Sᴇɴᴅ ᴜʀʟ | Nᴇᴡ ɴᴀᴍᴇ.ᴍᴋᴠ**
 
-𖣔 Sᴇʟᴇᴄᴛ ᴛʜᴇ ᴅᴇsɪʀᴇᴅ ᴏᴘᴛɪᴏɴ.
+𖣔 Sᴇʟᴇᴄᴛ ᴛʜᴇ ᴅᴇsɪʀᴇᴅ ᴏᴘᴛɪᴏɴ。
 
 𖣔 Usᴇ `/caption` ᴛᴏ sᴇᴛ ᴄᴀᴘᴛɪᴏɴ ᴀs Rᴇᴘʟʏ ᴛᴏ ᴍᴇᴅɪᴀ
-
 """
-    
+
     ABOUT_TEXT ="""
 ╭───────────⍟
 ├📛 **Mʏ Nᴀᴍᴇ** : URL Uᴘʟᴏᴀᴅᴇʀ Bᴏᴛ
 ├📢 **Fʀᴀᴍᴇᴡᴏʀᴋ** : <a href=https://docs.pyrogram.org/>PʏʀᴏBʟᴀᴄᴋ 2.6.8</a>
 ├💮 **Lᴀɴɢᴜᴀɢᴇ** : <a href=https://www.python.org>Pʏᴛʜᴏɴ 3.13.2</a>
 ├💾 **Dᴀᴛᴀʙᴀsᴇ** : <a href=https://cloud.mongodb.com>MᴏɴɢᴏDB</a>
-├🚨 **Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ** : <a href=https://t.me/NT_BOTS_SUPPORT>Nᴛ Sᴜᴘᴘᴏʀᴛ</a>
-├🥏 **Cʜᴀɴɴᴇʟ** : <a href=https://t.me/NT_BOT_CHANNEL>Nᴛ Bᴏᴛ Cʜᴀɴɴᴇʟ</a>
-├👨‍💻 **Cʀᴇᴀᴛᴇʀ** :  @NT_BOT_CHANNEL
+├🚨 **Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ** : <a href=https://t.me/NY_BOTS_SUPPORT>NY Sᴜᴘᴘᴏʀᴛ</a>
+├🥏 **Cʜᴀɴɴᴇʟ** : <a href=https://t.me/NY_BOTS>NY Bᴏᴛ Cʜᴀɴɴᴇʟ</a>
+├👨‍💻 **Cʀᴇᴀᴛᴇʀ** :  @NY_BOTS
 ╰───────────────⍟
 """
-
 
     PROGRESS = """
 ┣📦 Pʀᴏɢʀᴇꜱꜱ : {0}%
@@ -52,7 +49,6 @@ Usᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍ
 
     PROGRES = """
 `{}`\n{}"""
-
 
     INFO_TEXT = """
 ╭──────────────〄
@@ -67,52 +63,56 @@ Usᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍ
 ╰──────────────────〄
 """
 
-
     START_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('🛠️ SETTINGS', callback_data='OpenSettings')
+            InlineKeyboardButton('🛠️ SETTINGS', callback_data='OpenSettings')
         ],[
-        InlineKeyboardButton('🤝 HELP', callback_data='help'),
-        InlineKeyboardButton('🎯 ABOUT', callback_data='about')
+            InlineKeyboardButton('🤝 HELP', callback_data='help'),
+            InlineKeyboardButton('🎯 ABOUT', callback_data='about')
         ],[
-        InlineKeyboardButton('⛔ CLOSE', callback_data='close')
+            InlineKeyboardButton('⛔ CLOSE', callback_data='close')
         ]]
     )
+
     HELP_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('🛠️ SETTINGS', callback_data='OpenSettings')
+            InlineKeyboardButton('🛠️ SETTINGS', callback_data='OpenSettings')
         ],[
-        InlineKeyboardButton('🔙 BACK', callback_data='home'),
-        InlineKeyboardButton('🎯 ABOUT', callback_data='about')
+            InlineKeyboardButton('🔙 BACK', callback_data='home'),
+            InlineKeyboardButton('🎯 ABOUT', callback_data='about')
         ],[
-        InlineKeyboardButton('⛔ CLOSE', callback_data='close')
+            InlineKeyboardButton('⛔ CLOSE', callback_data='close')
         ]]
     )
+
     ABOUT_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('🛠️ SETTINGS', callback_data='OpenSettings')
+            InlineKeyboardButton('🛠️ SETTINGS', callback_data='OpenSettings')
         ],[
-        InlineKeyboardButton('🔙 BACK', callback_data='home'),
-        InlineKeyboardButton('🤝 HELP', callback_data='help')
+            InlineKeyboardButton('🔙 BACK', callback_data='home'),
+            InlineKeyboardButton('🤝 HELP', callback_data='help')
         ],[
-        InlineKeyboardButton('⛔ CLOSE', callback_data='close')
+            InlineKeyboardButton('⛔ CLOSE', callback_data='close')
         ]]
     )
+
     PLANS_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('🎯 ABOUT', callback_data='about')
+            InlineKeyboardButton('🎯 ABOUT', callback_data='about')
         ],[
-        InlineKeyboardButton('🔙 BACK', callback_data='home'),
-        InlineKeyboardButton('🤝 HELP', callback_data='help')
+            InlineKeyboardButton('🔙 BACK', callback_data='home'),
+            InlineKeyboardButton('🤝 HELP', callback_data='help')
         ],[
-        InlineKeyboardButton('⛔ CLOSE', callback_data='close')
-        ]]
-   )
-    BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('⛔ Close', callback_data='close')
+            InlineKeyboardButton('⛔ CLOSE', callback_data='close')
         ]]
     )
+
+    BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+
     INCORRECT_REQUEST = """Eʀʀᴏʀ"""
     DOWNLOAD_FAILED = "🔴 Eʀʀᴏʀ 🔴"
     TEXT = "Sᴇɴᴅ ᴍᴇ ʏᴏᴜʀ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ"
@@ -120,7 +120,7 @@ Usᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍ
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     FORMAT_SELECTION = "<b>Sᴇʟᴇᴄᴛ Yᴏᴜʀ Fᴏʀᴍᴀᴛ 👇</b>\n"
-    SET_CUSTOM_USERNAME_PASSWORD = """<b>🎥 Vɪᴅᴇᴏ = Uᴘʟᴏᴀᴅ As Sᴛʀᴇᴀᴍʙʟᴇ</b>\n\n<b>📂 Fɪʟᴇ = Uᴘʟᴏᴀᴅ As Fɪʟᴇ</b>\n\n<b>👮‍♂ Pᴏᴡᴇʀᴇᴅ Bʏ :</b> @NT_BOT_CHANNEL"""
+    SET_CUSTOM_USERNAME_PASSWORD = """<b>🎥 Vɪᴅᴇᴏ = Uᴘʟᴏᴀᴅ As Sᴛʀᴇᴀᴍʙʟᴇ</b>\n\n<b>📂 Fɪʟᴇ = Uᴘʟᴏᴀᴅ As Fɪʟᴇ</b>\n\n<b>👮‍♂ Pᴏᴡᴇʀᴇᴅ Bʏ :</b> @NY_BOTS"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
     DOWNLOAD_START = "📥 Downloading... 📥\n\nFile Name: {}"
     UPLOAD_START = "📤 Uploading... 📤"
@@ -134,7 +134,7 @@ Usᴇ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴋɴᴏᴡ ʜᴏᴡ ᴛᴏ ᴜsᴇ ᴍ
     NO_CUSTOM_THUMB_NAIL_FOUND = "ɴᴏ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ"
     NO_VOID_FORMAT_FOUND = "ERROR... <code>{}</code>"
     FILE_NOT_FOUND = "Error, File not Found!!"
-    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "Join : @NT_BOT_CHANNEL \n For the list of Telegram bots. "
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "Join : @NY_BOTS \n For the list of Telegram bots. "
     ADD_CAPTION_HELP = """Select an uploaded file/video or forward me <b>Any Telegram File</b> and just write the text you want to be on the file <b>as a reply to the file</b> and the text you wrote will be attached as the caption! 🤩
     
 Example: <a href='https://te.legra.ph/file/ecf5297246c5fb574d1a0.jpg'>See This!</a> 👇"""
